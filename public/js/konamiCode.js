44763c3fad2c257493db09be60535fdc01451e37
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2023-12-07 21:04:11 +0100

    Konami code working

--- public/js/konamiCode.js
+++ public/js/konamiCode.js
@@ -1,5 +1,3 @@
-var keys='';
-
 var input = new Array();
 
 var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
@@ -7,10 +5,15 @@ var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 document.onkeyup = function(e) {
     get = window.event?event:e;
     key = get.keyCode?get.keyCode:get.charCode;
-    input[input.length+1] = key
+    input[input.length] = key
+
+    console.log(JSON.stringify(konamiCode) === JSON.stringify(input));
+    console.log(JSON.stringify(konamiCode));
+    console.log(JSON.stringify(input));
+
+    if (JSON.stringify(konamiCode) === JSON.stringify(input)){
+        console.log("KONAMI CODE REUSSI");
+    }
+
     console.log(input)
-    keys+=key;
 }
-window.setInterval(function(){
-    console.log("ratio")
-}, 100000);
