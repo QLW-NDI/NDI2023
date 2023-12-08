1955d1851ddf3693c5be98c9893f1a1ab73d07f7
Mika <mika.chuewaye@gmail.com>
2023-12-08 03:35:14 +0100

    js code cleaning

--- public/js/ndi.js
+++ public/js/ndi.js
@@ -1,21 +1,16 @@
 let i = 0;
 let leftAnswer = document.getElementById("left-answer");
 let rightAnswer = document.getElementById("right-answer");
-
 let answers = document.getElementById("answers");
 let explanation = document.getElementById("explanation");
 let mainContent = document.getElementById("main-content");
-
 let next = document.getElementById("next");
 let contentBox = document.getElementById("content-box");
 let answerBox = document.getElementById("answer-box");
 
-
-
 function resizeAnswers(leftPercentage, rightPercentage) {
     leftAnswer.style.pointerEvents = "none";
     rightAnswer.style.pointerEvents = "none";
-
     leftAnswer.style.width = leftPercentage;
     leftAnswer.innerHTML = "<h1>"+leftPercentage+"</h1>";
     rightAnswer.style.width = rightPercentage;
@@ -24,19 +19,15 @@ function resizeAnswers(leftPercentage, rightPercentage) {
 
 function showExplanation() {
     let children = explanation.children;
-
     answers.style.flex = "0 1 50px";
-    // mainContent.style.margin = "0 10vw 0 10vw";
     setTimeout(function(){
         mainContent.style.height = "100%";
-        // explanation.style.flex = "0 1 500px";
         explanation.style.padding = "20px 40px 20px 40px";
         explanation.style.height = "auto";
         contentBox.style.flex = "none";
         answerBox.style.width = "100%";
         Array.from(children).forEach(child => {
             child.style.fontSize = "initial"
-            // child.style.visibility = "visible";
         });
         setTimeout(function() {
             answerBox.style.width = "calc(100% - 100px)";
@@ -49,14 +40,10 @@ function showExplanation() {
 
 function resetAnswers() {
     let children = explanation.children;
-
-    // answerBox.style.width = "100%";
     next.style.width = "0";
     next.style.marginLeft = "0";
     next.style.fontSize = "0";
-
     answerBox.style.width = "100%";
-
     setTimeout(function(){
         contentBox.style.flex= "1 1 auto";
         Array.from(children).forEach(child => {
@@ -66,35 +53,13 @@ function resetAnswers() {
         explanation.style.height = "0";
         mainContent.style.height = "calc(100% - 10vw)";
         answers.style.flex= "1 1 auto";
-
         leftAnswer.style.width = "50%";
         leftAnswer.innerHTML = "<h1>"+"réponse 1"+"</h1>";
         rightAnswer.style.width = "50%";
         rightAnswer.innerHTML = "<h1>"+"réponse 2"+"</h1>";
-
         leftAnswer.style.pointerEvents = "auto";
         rightAnswer.style.pointerEvents = "auto";
-    }, 500)
-
-    // setTimeout(function() {
-    //
-    //
-    //     answerBox.style.width = "100%";
-    //     contentBox.style.flex = "1 1 auto";
-    //
-    //     mainContent.style.height = "calc(100% - 10vw)";
-    //
-    //
-    //     // setTimeout(function(){
-    //     //     answers.style.flex= "1 1 auto";
-    //     //
-    //     //
-    //     // }, 500);
-    // }, 1000);
-
-
-
-
+    }, 500);
 }
 
 const handleOnLeftAnswerClick = e => {
