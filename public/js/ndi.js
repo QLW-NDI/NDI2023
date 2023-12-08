d968adbc4b120083ec1adff8419de94fc299f5e4
Mika <mika.chuewaye@gmail.com>
2023-12-08 02:02:59 +0100

    add next button to ndi.html

--- public/js/ndi.js
+++ public/js/ndi.js
@@ -6,6 +6,12 @@ let answers = document.getElementById("answers");
 let explanation = document.getElementById("explanation");
 let mainContent = document.getElementById("main-content");
 
+let next = document.getElementById("next");
+let contentBox = document.getElementById("content-box");
+let answerBox = document.getElementById("answer-box");
+
+
+
 function resizeAnswers(leftPercentage, rightPercentage) {
     leftAnswer.style.width = leftPercentage;
     leftAnswer.innerHTML = "<h1>"+leftPercentage+"</h1>";
@@ -14,12 +20,30 @@ function resizeAnswers(leftPercentage, rightPercentage) {
 }
 
 function showExplanation() {
+    let children = explanation.children;
+
+    leftAnswer.style.pointerEvents = "none";
+    rightAnswer.style.pointerEvents = "none";
+
     answers.style.flex = "0 1 50px";
     // mainContent.style.margin = "0 10vw 0 10vw";
     setTimeout(function(){
         mainContent.style.height = "100%";
-        explanation.style.flex = "0 1 500px";
-        // explanation.childNodes
+        // explanation.style.flex = "0 1 500px";
+        explanation.style.padding = "20px 40px 20px 40px";
+        explanation.style.height = "auto";
+        contentBox.style.flex = "none";
+        answerBox.style.width = "100%";
+        Array.from(children).forEach(child => {
+            child.style.fontSize = "initial"
+            child.style.visibility = "visible";
+        });
+        setTimeout(function() {
+            answerBox.style.width = "calc(100% - 100px)";
+            next.style.width = "100px";
+            next.style.marginLeft = "10px";
+            next.style.fontSize = "xxx-large";
+        }, 2000);
     }, 500);
 }
 
